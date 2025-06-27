91899fc72344f867f16553a9d0b30e0e803b65a2
divyanshuaaliwal <divyanshuaaliwal@gmail.com>
2025-06-27 13:44:09 +0530

    Not Found

--- app/not-found.js
+++ app/not-found.js
@@ -10,7 +10,7 @@ export default function NotFound() {
             <h1 className={styles.errorCode}>404</h1>
             <p className={styles.title}>Oops! Page not found</p>
             <p className={styles.description}>
-                The page you're looking for doesn’t exist or has been moved.
+                The page you are looking for doesn’t exist or has been moved.
             </p>
 
             <Link href="/" className={styles.backButton}>
